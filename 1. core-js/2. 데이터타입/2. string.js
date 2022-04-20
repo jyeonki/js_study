2a5cc5fce782335298e9c04dbb8a1dcd802a35aa
jyeonki <kay8825@naver.com>
2022-04-20 18:19:24 +0900

    [220420] JavaScript 데이터타입(number, string) 학습

---
+++ 1. core-js/2. 데이터타입/2. string.js
@@ -0,0 +1,28 @@
+
+var greeting;
+greeting = 'hello';
+greeting = "안녕하세요";
+greeting = `콘니치와`; // 왼쪽은 무조건 변수, 변수에 '' 넣으면 안됨
+
+console.log(typeof greeting);
+
+// var str1 = "그는 나에게 "메롱"이라고 하고 도망갔다."; //
+var str1 = '그는 나에게 "메롱"이라고 하고 도망갔다.';
+console.log(str1);
+
+var str2 = "Let's Go!";
+console.log(str2);
+
+var str3 = "Let's Go! \"together\""; 
+console.log(str3);
+
+// 경로표시: 리눅스- /, 윈도우- \
+// var str4 = 'E:\temp\new.png'; // \t: tab, \n: new line
+var str4 = 'E:\\temp\\new.png';
+console.log(str4);
+
+var str5 = '멍멍이\n\n\n\n야옹이';
+console.log(str5);
+
+var str6 = '멍멍이\t\t\t야옹이';
+console.log(str6);
